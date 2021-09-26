36574f3b0426f0aa3909f77c86ab50e945ed3681
shamiik <shamikdey7@gmail.com>
2021-09-26 13:06:44 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import Header from './components/Header/Header';
 
 function App() {
+  useEffect(() => {
+    fetch('./realdb.JSON')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, [])
   return (
     <div>
-
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>The Greatest Programmer</h1>
+            <p>There is a conference about them</p>
+            <h2>Total Budget: <span className='fw-bold'>120 Million</span></h2>
+        </div>
+    );
+};
+
+export default Header;
